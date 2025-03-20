e28be27b828f93c6be620adc1aae54760e5db72a
Arthur Silva <arthur.silva443@gmail.com>
2025-03-20 20:28:11 -0300

    adicionando classe ao gitignore e testando operadores

---
+++ Operadores/Operadores.java
@@ -0,0 +1,12 @@
+package Operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int primeiroNumero = 10;
+        int segundoNumero = 15;
+
+        int soma = primeiroNumero + segundoNumero;
+
+        System.out.println(soma);
+    }
+}
