00829817bf19475f2f2f06d43996aa6152aed912
Arthur Silva <arthur.silva443@gmail.com>
2025-03-20 20:21:01 -0300

    adicionando elementos ao .gitignore e testando tipos de variaveis

--- TiposDeVariaveis/src/Main.java
+++ TiposDeVariaveis/src/Main.java
@@ -1,5 +1,17 @@
+package TiposDeVariaveis.src;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String meuNome = "Arthur";
+
+        double salarioMinimo = 2500;
+        short salario = 10;
+        int salario2 = 100;
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }
