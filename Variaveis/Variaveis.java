f452b29abec5800f80bc19c57be4f28953ffdc73
Arthur Silva <arthur.silva443@gmail.com>
2025-03-20 20:44:45 -0300

    tipos de variaveis

---
+++ Variaveis/Variaveis.java
@@ -0,0 +1,20 @@
+package Variaveis;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        byte muintoCurto = 0;
+        short curto = 1;
+        int padrao = 10;
+        long longo = 100;
+
+        double comVirgula = 100.00;
+        float comPontoFlutuante = 100.00F;
+
+        char caracter = 85;
+
+        String texto = "Um texto";
+
+        boolean verdadeiroFalso = true;
+
+    }
+}
