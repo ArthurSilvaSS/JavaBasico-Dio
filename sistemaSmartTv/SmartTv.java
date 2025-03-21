0308298cec821be71312243f03852d512e8223c8
Arthur Silva <arthur.silva443@gmail.com>
2025-03-21 18:13:22 -0300

    trabalhando com metodos

--- sistemaSmartTv/SmartTv.java
+++ sistemaSmartTv/SmartTv.java
@@ -1,4 +1,47 @@
 package sistemaSmartTv;
 
 public class SmartTv {
+    // Declarando Variaveis
+
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    // Metodos
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+    public void aumentarVolume() {
+        //volume = volume + 1;
+        volume++;
+        System.out.println("A justando o volume da tv para -> " + volume);
+    }
+
+    public void diminuirVolume() {
+        //volume = volume -1;
+        volume--;
+        System.out.println("A justando o volume da tv para -> " + volume);
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+        System.out.println("Novo canal ajustado " + canal);
+    }
+
+    public void aumentarCanal() {
+        canal++;
+        System.out.println("A justando o canal da tv para -> " + canal);
+    }
+
+    public void diminuirCanal() {
+        canal--;
+        System.out.println("A justando o canal da tv para -> " + canal);
+    }
+
+
 }
--- sistemaSmartTv/Usuario.java
+++ sistemaSmartTv/Usuario.java
@@ -1,4 +1,34 @@
 package sistemaSmartTv;
 
 public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("----------ESTADO INICIAL----------");
+
+        System.out.println("A Tv esta Ligada? " + smartTv.ligada);
+        System.out.println("Qual o canal atual? " + smartTv.canal);
+        System.out.println("Qual o volume atual? " + smartTv.volume);
+
+        System.out.println("----------------------------------");
+
+        smartTv.ligar();
+        System.out.println("Estado atual da tv -> " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("Estado atual da Tv -> " + smartTv.ligada);
+
+        System.out.println("----------------------------------");
+
+        smartTv.aumentarVolume();
+        smartTv.diminuirVolume();
+
+        System.out.println("----------------------------------");
+
+        smartTv.mudarCanal(15);
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+
+
+    }
 }
